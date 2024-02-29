6ab6eb177bf003b30879ccdc55e328e1fdbea4d1
Baylor <942895666@qq.com>
2024-02-29 11:20:06 +0800

    binary search problems added

--- binarySearch.h
+++ binarySearch.h
@@ -29,6 +29,95 @@ public:
     }
 };
 
+//35. Search Insert Position
+class Solution35 {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int ans = nums.size();
+        int left = 0;
+        int right = nums.size() - 1;
+        int mid = 0;
+
+        while(left <= right) {
+            mid = ((right - left) >> 1) + left;
+            if (target <= nums[mid]) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
+
+//374. Guess Number Higher or Lower
+class Solution374 {
+    int guess(int num);
+public:
+    int guessNumber(int n) {
+        long long left=1,right=n;
+        while(left<=right)
+        {
+            long long mid=(left+right)/2;
+            int res=guess(mid);
+            if(res==0)
+                return mid;
+            else if(res==-1)
+                right=mid-1;
+            else
+                left=mid+1;
+        }
+        return -1;
+    }
+};
+
+//69. Sqrt(x)
+class Solution69 {
+public:
+    int mySqrt(int x) {
+        int left = 0;
+        int right = x;
+        long long mid = 0;
+
+        while (left<=right) {
+            mid = left + ((right - left) / 2);
+            //int tmp = mid * mid;
+            if (mid * mid == x || (mid * mid < x && (mid+1)*(mid+1)>x))
+                return mid;
+            else if (mid * mid < x)
+                left = mid+1;
+            else
+                right = mid - 1;
+        }
+        return -1;
+    }
+};
+
+//162. Find Peak Element
+class Solution162 {
+public:
+    int findPeakElement(vector<int>& nums) {
+        if (nums.size()==1)
+            return 0;
+        int left = 0;
+        int right = nums.size();
+
+        while (left < right) {
+            int mid = left + ((right-left)/2);
+            if (((mid+1<right && nums[mid]>nums[mid+1]) || mid+1>=right) &&
+                ((mid-1>=left && nums[mid]>nums[mid-1]) || mid-1<left)) {
+                return mid;
+            } else if (nums[mid] >= nums[mid-1]) {
+                left = mid+1;
+            } else {
+                right = mid;
+            }
+        }
+        return -1;
+    }
+};
+
 //74. search a 2d matrix
 class Solution74 {
 public:
@@ -246,4 +335,32 @@ public:
         return result;
     }
 };
+
+//34. Find First and Last Position of Element in Sorted Array
+class Solution {
+public:
+    int binarySearch(vector<int>& nums, int target, bool lower) {
+        int left = 0, right = (int)nums.size() - 1, ans = (int)nums.size();
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx < nums.size() && nums[leftIdx] == target && nums[rightIdx] == target) {
+            return vector<int>{leftIdx, rightIdx};
+        }
+        return vector<int>{-1, -1};
+    }
+};
+
 #endif //NEETCODE150_BINARYSEARCH_H
