626cbf6b72dd8d0b5073fc66e024f78158f971dc
Baylor <942895666@qq.com>
2024-03-13 11:58:21 +0800

    stack problem completed

--- stack.h
+++ stack.h
@@ -194,7 +194,7 @@ public:
 };
 
 //394. Decode String
-class Solution394 {
+class Solution394_stack {
 public:
     string decodeString(string s) {
         stack<int> multi_stack;
@@ -264,6 +264,201 @@ public:
     }
 };
 
+//32. Longest Valid Parentheses
+class Solution32 {
+public:
+    int longestValidParentheses(string s) {
+        int maxans = 0;
+        stack<int> stk;
+        stk.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                stk.push(i);
+            } else {
+                stk.pop();
+                if (stk.empty()) {
+                    stk.push(i);
+                } else {
+                    maxans = max(maxans, i - stk.top());
+                }
+            }
+        }
+        return maxans;
+    }
+};
 
+//225. Implement Stack using Queues
+class MyStack {
+    queue<int> q1;
+    queue<int> q2;
+public:
+    /** Initialize your data structure here. */
+    MyStack() = default;
 
+    /** Push element x onto stack. */
+    void push(int x) {
+        q2.push(x);
+        while (!q1.empty()) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        swap(q1, q2);
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int x = q1.front();
+        q1.pop();
+        return x;
+    }
+
+    /** Get the top element. */
+    int top() {
+        return q1.front();
+    }
+
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return q1.empty();
+    }
+};
+
+//232. Implement Queue using Stacks
+class MyQueue {
+public:
+    stack<int> ins;
+    stack<int> outs;
+    void in2out () {
+        while (!ins.empty()) {
+            outs.push(ins.top());
+            ins.pop();
+        }
+    }
+    MyQueue() =default;
+
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        ins.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        if (outs.empty()) {
+            in2out();
+        }
+        int x = outs.top();
+        outs.pop();
+        return x;
+    }
+
+    /** Get the front element. */
+    int peek() {
+        if (outs.empty()) {
+            in2out();
+        }
+        return outs.top();
+    }
+
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return ins.empty() && outs.empty();
+    }
+};
+
+//341. Flatten Nested List Iterator
+ class NestedInteger {
+  public:
+    // Return true if this NestedInteger holds a single integer, rather than a nested list.
+    bool isInteger() const;
+
+    // Return the single integer that this NestedInteger holds, if it holds a single integer
+    // The result is undefined if this NestedInteger holds a nested list
+    int getInteger() const;
+
+    // Return the nested list that this NestedInteger holds, if it holds a nested list
+    // The result is undefined if this NestedInteger holds a single integer
+    const vector<NestedInteger> &getList() const;
+};
+class NestedIterator {
+    deque<int> queue;
+public:
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        for (auto list: nestedList)
+            dfs(list);
+    }
+
+    int next() {
+        if (hasNext()) {
+            int x = queue.front();
+            queue.pop_front();
+            return x;
+        }
+        else
+            return -1;
+    }
+
+    bool hasNext() {
+        return !queue.empty();
+    }
+
+    void dfs(NestedInteger list) {
+        if (list.isInteger()) {
+            queue.push_back(list.getInteger());
+        }
+        else {
+            for (auto itor: list.getList()) {
+                dfs(itor);
+            }
+        }
+    }
+
+};
+
+//445. Add Two Numbers II
+class Solution445 {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        stack<int> s1;
+        stack<int> s2;
+        ListNode* node = l1;
+        while (node != nullptr) {
+            s1.push(node->val);
+            node = node->next;
+        }
+        node = l2;
+        while (node != nullptr) {
+            s2.push(node->val);
+            node = node->next;
+        }
+
+        int carry = 0;
+        int tmp1 =0;
+        int tmp2 =0;
+        ListNode* next = nullptr;
+        ListNode* newnode = nullptr;
+        while (!s1.empty() || !s2.empty() || carry != 0) {
+            newnode = new ListNode;
+            if (!s1.empty()) {
+                tmp1 = s1.top();
+                s1.pop();
+            }
+            else {
+                tmp1 = 0;
+            }
+
+            if (!s2.empty()) {
+                tmp2 = s2.top();
+                s2.pop();
+            }
+            else {
+                tmp2 = 0;
+            }
+            newnode->val = (tmp1 + tmp2 + carry) % 10;
+            carry = (tmp1 + tmp2 + carry) / 10;
+            newnode->next = next;
+            next = newnode;
+        }
+        return newnode;
+    }
+};
 #endif //NEETCODE150_STACK_H
